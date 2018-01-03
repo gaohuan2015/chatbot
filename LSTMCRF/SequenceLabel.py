231e471c077bcc777f02e37d691d0388e6c6a647
gaohu <53857658@qq.com>
2018-01-03 22:48:12 +0800

    LSTM+CRF加入了bilstm完成了基本算法

--- LSTMCRF/SequenceLabel.py
+++ LSTMCRF/SequenceLabel.py
@@ -16,17 +16,23 @@ with tf.Graph().as_default():
   with tf.Session() as session:
     x_t = tf.constant(x)
     y_t = tf.constant(y)
-    equence_lengths_t = tf.constant(sequence_lengths)
-    weights = tf.get_variable("weights", [num_features, num_tags])
-    matricized_x_t = tf.reshape(x_t, [-1, num_features])
+    sequence_lengths_t = tf.constant(sequence_lengths)
+    outputs,_ = tf.nn.bidirectional_dynamic_rnn(tf.nn.rnn_cell.LSTMCell(num_features),
+                                                       tf.nn.rnn_cell.LSTMCell(num_features),
+                                                       x_t,
+                                                       dtype=tf.float32,
+                                                       sequence_length= sequence_lengths_t)
+    output = tf.concat([outputs[0],outputs[1]],2)
+    weights = tf.get_variable("weights", [num_features*2, num_tags])
+    matricized_x_t = tf.reshape(outputs, [-1, num_features*2])
     matricized_unary_scores = tf.matmul(matricized_x_t, weights)
     unary_scores = tf.reshape(matricized_unary_scores, [num_examples, num_words, num_tags])
-    log_likelihood, transition_params = tf.contrib.crf.crf_log_likelihood(unary_scores, y_t, equence_lengths_t)
+    log_likelihood, transition_params = tf.contrib.crf.crf_log_likelihood(unary_scores, y_t, sequence_lengths_t)
     loss = tf.reduce_mean(-log_likelihood)
     train_op = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
 
     session.run(tf.global_variables_initializer())
-    for i in range(4000):
+    for i in range(2000):
         tf_unary_scores, tf_transition_params, _ = session.run(
               [unary_scores, transition_params, train_op])
         if i % 100 == 0:
